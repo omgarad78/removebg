93675943b9d5d58d54af66e1b2f1ea41ef1e0a80
omgarad78 <omgdsc78@gmail.com>
2024-11-06 19:26:28 +0530

    this is usd to remove your baground

---
+++ removebg.py
@@ -0,0 +1,14 @@
+from rembg import remove
+import easygui
+from PIL import Image
+
+inputPath=easygui.fileopenbox(title='Select the Image')
+outputPath = easygui.filesavebox("Choose where to save file")
+
+input=Image.open(inputPath)
+output=remove(input)
+output.save(f"{outputPath}.png")
+
+
+
+
